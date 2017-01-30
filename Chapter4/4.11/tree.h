28a2ca06838da00e65ec60024328778ecd90dba7
Zhiqiao Xu <xuzhiqiao97@gmail.com>
2017-01-30 22:22:19 +0800

    Create tree.h

---
+++ Chapter4/4.11/tree.h
@@ -0,0 +1,20 @@
+typedef int ElementType;
+#define SpaceSize 100  
+#ifndef _Tree_H
+#define _Tree_H
+
+typedef int PtrToNode;
+typedef PtrToNode Position;
+typedef PtrToNode SearchTree;
+
+void initializeCursorSpace(void);
+
+void makeEmpty(SearchTree t);
+Position find(ElementType X, SearchTree t);
+Position findMin(SearchTree t);
+Position findMax(SearchTree t);
+SearchTree insert(ElementType X, SearchTree t);
+SearchTree Delete(ElementType X, SearchTree t);
+ElementType Retrieve(Position p);
+void Dir(SearchTree t);
+#endif
