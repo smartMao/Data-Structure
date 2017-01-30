637b4ba3d602382f52ce01e2206e329aca38f70c
Zhiqiao Xu <xuzhiqiao97@gmail.com>
2017-01-30 15:35:45 +0800

    Create tree.h

---
+++ Chapter4/4.10/tree.h
@@ -0,0 +1,22 @@
+#ifndef _Tree_H
+
+struct TreeNode;
+
+typedef struct TreeNode *SearchTree;
+typedef struct TreeNode *Position;
+typedef int ElementType;
+
+SearchTree MakeEmpty(SearchTree T);
+Position Find(ElementType Elem, SearchTree T);
+Position FindMin(SearchTree T);
+Position FindMax(SearchTree T);
+SearchTree Insert(ElementType Elem, SearchTree T);
+SearchTree Delete(ElementType Elem, SearchTree T);
+
+#endif 
+
+struct TreeNode {
+	ElementType Element;
+	SearchTree Left;
+	SearchTree Right;
+} TreeNode;
