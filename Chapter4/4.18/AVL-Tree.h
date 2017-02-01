d179e0ca70908590ac03d4aae6699b71dc80ed19
Zhiqiao Xu <xuzhiqiao97@gmail.com>
2017-02-01 20:18:49 +0800

    Create AVL-Tree.h

---
+++ Chapter4/4.18/AVL-Tree.h
@@ -0,0 +1,11 @@
+
+
+struct treeNode;
+typedef struct treeNode *AvlTree;
+typedef struct treeNode *Position;
+typedef int ElementType;
+
+AvlTree insert(ElementType elem, AvlTree t);
+
+int height(AvlTree t);
+ElementType Max(ElementType a, ElementType b);
