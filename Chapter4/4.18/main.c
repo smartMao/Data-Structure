4dbc040699e6ab4e1066d6302aacd16e3fceddac
Zhiqiao Xu <xuzhiqiao97@gmail.com>
2017-02-01 20:18:04 +0800

    实现 - AVL树 单旋转 和 双旋转

---
+++ Chapter4/4.18/main.c
@@ -0,0 +1,24 @@
+#include"AVL-Tree.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+// 实现 - AVL树 单旋转 和 双旋转
+
+int main() {
+	AvlTree t = NULL;
+
+	//int array[100] = { 50, 40, 30, 20, 10, 5 }; // 单旋转
+	//int array[100] = { 10 , 6, 4}; // 单旋转
+	//int array[100] = { 40, 30 , 50, 31, 32}; // 单旋转
+	//int array[100] = { 50, 60 , 40, 30, 20}; // 单旋转
+	//int array[100] = { 10 , 4 ,6}; // 双旋转
+	//int array[100] = { 10 , 16 ,15}; // 双旋转
+	//int array[100] = { 50, 20, 70, 10, 30, 60, 80, 4, 6}; // 双旋转
+	int array[100] = {52,61,81,49,75,63,85,26,89,68,78,56,15};
+
+	for (int i = 0; i < 100; i++) {
+		//t= insert(rand() % 100, t);
+		t= insert(array[i], t);
+	}
+		
+}
